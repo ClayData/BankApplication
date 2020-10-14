1384166d73bf288e7207ba8d5b71c8c2d7c60c6f
ClayData <claytonortiz96@gmail.com>
2020-10-13 19:16:23 -0500

    added question input in bankingapp class

---
+++ src/banking/BankApplication.java
@@ -0,0 +1,25 @@
+package banking;
+
+import java.util.Scanner;
+
+public class BankApplication {
+	
+	public void customerEntry() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter customer name: ");
+		String name = sc.next();
+		System.out.println("Enter customer gender: ");
+		String gender = sc.next();
+		System.out.println("Enter customer salary: ");
+		int salary = sc.nextInt();
+		System.out.println("Enter customer email: ");
+		String contact = sc.next();
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
